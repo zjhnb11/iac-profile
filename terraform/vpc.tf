fe1b8eecc41ef147fd1d15f5181fba0fa3057fae
zhaojo <st180794@stud.uni-stuttgart.de>
2025-04-16 13:00:22 +0200

    changes

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,10 +2,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.2"
 
-  name = "vprofile-eks"
+  name = "profile-eks"
 
   cidr = "172.20.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  # azs to choose first 3 availability zones in the region
 
   private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
   public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
